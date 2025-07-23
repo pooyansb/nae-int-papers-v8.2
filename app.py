4857812179fa0b3dbaa59ce60549b5b934077aeb
pooyansb <79424335+pooyansb@users.noreply.github.com>
2025-07-23 09:58:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from routes.home import home_bp
 from routes.sankey import sankey_bp
 from routes.search import search_bp
+from routes.about import about_bp
 from config import Config
 
 def create_app():
@@ -11,6 +12,7 @@ def create_app():
     app.register_blueprint(home_bp)
     app.register_blueprint(sankey_bp)
     app.register_blueprint(search_bp)
+    app.register_blueprint(about_bp)
 
     return app
 
