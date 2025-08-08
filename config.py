4293b4ea1ef8449b3b26cc9a8bf613fe37defb07
pooyans <p.sahbaee@gmail.com>
2025-08-08 14:45:02 -0400

    Update config.py and add new CSV for Sankey diagram

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY       = os.environ.get("SECRET_KEY", "replace-this-in-prod")
-    DATA_CSV         = os.path.join(BASE_DIR, "processed_data_PublishedAlphaPapers_PubMed_RadiologyReview_UsePapersDirectly_V1_Sankey.csv")
+    DATA_CSV         = os.path.join(BASE_DIR, "processed_data_PublishedAlphaPapers_PubMed_RadiologyReview_UsePapersDirectly_V2_Sankey.csv")
     FAISS_INDEX      = os.path.join(BASE_DIR, "processed_data_PublishedAlphaPapers_PubMed_RadiologyReview_UsePapersDirectly_V2.index")
     KEY_TERMS_FILE   = os.path.join(BASE_DIR, "key_terms.json")
     OPENAI_API_KEY   = os.environ.get("OPENAI_API_KEY")
