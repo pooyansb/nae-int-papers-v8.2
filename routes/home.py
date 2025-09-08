50e516c10167a245ec3fc0164418f216fd0dbf3d
pooyansb <79424335+pooyansb@users.noreply.github.com>
2025-09-08 09:35:34 -0400

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/", methods=["GET"])
 def home():
-    return render_template("home.html")
+    #return render_template("home.html")
+    return redirect(url_for("sankey.sankey"))
